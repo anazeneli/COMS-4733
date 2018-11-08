48c0e95c95f8962478dd1e60e9589fbd9f2fd683
Ana Zeneli <az2498@columbia.edu>
2018-11-07 19:55:55 -0500

    Creating lab4 folder

---
+++ lab4/rrt.py
@@ -0,0 +1,107 @@
+# Program to load obstacle course for Lab 4 - RRT
+
+# usage:  python rrt.py obstacles_file start_goal_file
+
+
+from __future__ import division
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+import numpy as np
+import random, math
+
+def build_obstacle_course(obstacle_path, ax):
+    vertices = list()
+    codes = [Path.MOVETO]
+    with open(obstacle_path) as f:
+        quantity = int(f.readline())
+        lines = 0
+        for line in f:
+            coordinates = tuple(map(int, line.strip().split(' ')))
+            if len(coordinates) == 1:
+                codes += [Path.MOVETO] + [Path.LINETO]*(coordinates[0]-1) + [Path.CLOSEPOLY]
+                vertices.append((0,0)) #Always ignored by closepoly command
+            else:
+                vertices.append(coordinates)
+    vertices.append((0,0))
+    vertices = np.array(vertices, float)
+    path = Path(vertices, codes)
+    pathpatch = patches.PathPatch(path, facecolor='None', edgecolor='xkcd:violet')
+
+    ax.add_patch(pathpatch)
+    ax.set_title('Rapidly-exploring Random Tree')
+
+    ax.dataLim.update_from_data_xy(vertices)
+    ax.autoscale_view()
+    ax.invert_yaxis()
+
+    return path
+
+def add_start_and_goal(start_goal_path, ax):
+    start, goal = None, None
+    with open(start_goal_path) as f:
+        start = tuple(map(int, f.readline().strip().split(' ')))
+        goal  = tuple(map(int, f.readline().strip().split(' ')))
+
+    ax.add_patch(patches.Circle(start, facecolor='xkcd:bright green'))
+    ax.add_patch(patches.Circle(goal, facecolor='xkcd:fuchsia'))
+
+    return start, goal
+
+# helper function to draw RRT search path
+def draw_rand(start, rand):
+    x = [start[0], rand[0]]
+    y = [start[1], rand[1]]
+
+    for i in range(0, len(x), 2):
+        plt.plot(x[i:i+2], y[i:i+2])
+
+    ax.add_patch(patches.Circle(rand, facecolor='xkcd:red'))
+    plt.show()
+
+
+def readObs(filename):
+	# opening the file
+	obsFile = open(filename, 'r')
+
+	# dictionaray of obs in the file
+	obstacles = {}
+
+	# first line of the file is the number of objects
+	numObs = int(obsFile.readline());
+
+	# print "The number of obstacles is: ", numObs
+	# iterating over all the obstaces
+	for x in range(numObs):
+		# firstline of each obstacle is the number of edges
+		numEdges = int(obsFile.readline());
+		# print "the number of edges of:", x, "is", numEdges
+		# iterating ove all the edges
+		corners = []
+		for y in range(numEdges):
+
+			c = obsFile.readline();
+			# removing whitespace and storing the resulting words in array
+			c = c.split();
+			corners.append((float(c[0])/scale, float(c[1])/scale))
+		# print corners
+		# UPDATING THE dictionaray
+		obstacles[x] = corners
+
+	return obstacles
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('obstacle_path',
+                        help="File path for obstacle set")
+    parser.add_argument('start_goal_path',
+                        help="File path for obstacle set")
+    args = parser.parse_args()
+
+    fig, ax = plt.subplots()
+    path = build_obstacle_course(args.obstacle_path, ax)
+    start, goal = add_start_and_goal(args.start_goal_path, ax)
+
+    plt.show()
