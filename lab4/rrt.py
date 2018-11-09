2803933e082c801801374145e873d6c96bf73bdd
Ana Zeneli <az2498@columbia.edu>
2018-11-08 19:32:44 -0500

    Improving readability

--- lab4/rrt.py
+++ lab4/rrt.py
@@ -1,15 +1,12 @@
 # Program to load obstacle course for Lab 4 - RRT
-
 # usage:  python rrt.py obstacles_file start_goal_file
-
-
 from __future__ import division
+from line import Line
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 import numpy as np
 import random, math
-
 
 # global variables
 step_size = 50.0
@@ -20,7 +17,6 @@ xbottom = 0
 
 ytop = 600
 ybottom = 0
-
 
 def build_obstacle_course(obstacle_path, ax):
     vertices = list()
@@ -60,21 +56,7 @@ def add_start_and_goal(start_goal_path, ax):
 
     return start, goal
 
-# helper function to draw RRT search path
-def draw_rand(start, rand):
-    x = [start[0], rand[0]]
-    y = [start[1], rand[1]]
-
-    for i in range(0, len(x), 2):
-        plt.plot(x[i:i+2], y[i:i+2])
-
-    ax.add_patch(patches.Circle(rand, facecolor='xkcd:red'))
-    plt.show()
-
-
-
-# funciton that generates the ranomd configuration
-
+# function that generates the ranomd configuration
 def get_rand(pos):
     x = pos[0]
     y = pos[1]
@@ -96,136 +78,9 @@ def get_rand(pos):
     return [new_x, new_y]
 
     # print pow(new_x,2) + pow(new_y,2)
-    # assert 1==2
-
-
-
-class Line():
-    def __init__(self, arg1, arg2):
-        self.ep1 = arg1
-        self.ep2 = arg2
-        self.m = 0
-        self.c = 0
-        self.h = 0
-        self.v = 0
-        ## x values are equal so vertical line
-        if arg1[0] == arg2[0]:
-            self.type = 'v'
-            self.v = arg2[0]
-        elif arg1[1] == arg2[1]:
-            self.type = 'h'
-            self.h = arg2[1]
-        else:
-            self.type = 's'
-            self.m = (arg1[1]-  arg2[1])/(arg1[0] - arg2[0]);
-            self.c = arg1[1] - self.m*arg1[0]
-
-
-
-    def equality(self, lineCheck):
-        if (self.ep1[0] - lineCheck.ep1[0] == 0)&(self.ep1[1] - lineCheck.ep1[1] == 0):
-            return True;
-        if (self.ep2[0] - lineCheck.ep1[0] == 0)&(self.ep2[1] - lineCheck.ep1[1] == 0):
-            return True;
-
-        if (self.ep1[0] - lineCheck.ep2[0] == 0)&(self.ep1[1] - lineCheck.ep2[1] == 0):
-            return True;
-
-        if (self.ep2[0] - lineCheck.ep2[0] == 0)&(self.ep2[1] - lineCheck.ep2[1] == 0):
-            return True;
-
-        return False;
-
-
-    def intersect(self, lineCheck):
-
-        # checking if two points are common or not
-        if self.equality(lineCheck):
-            return False
-        # checking if the lines are parallel or not
-        if (self.type == lineCheck.type)&(self.m == lineCheck.m):
-            return False;
-
-
-        # taking care of easy cases
-        if (self.type == 'h'):
-            if (lineCheck.type == 'v'):
-                if (min(self.ep1[0],self.ep2[0]) < lineCheck.v)&(max(self.ep1[0],self.ep2[0]) > lineCheck.v):
-                    if (min(lineCheck.ep1[1],lineCheck.ep2[1]) < self.h)&(max(lineCheck.ep1[1],lineCheck.ep2[1]) > self.h):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            elif (lineCheck.type == 'h'):
-                return False
-            else:
-                # getting the intersect
-                tempX = float((self.h - lineCheck.c)/lineCheck.m)
-                if (min(self.ep1[0],self.ep2[0]) < tempX)&(max(self.ep1[0],self.ep2[0]) > tempX):
-                    if (min(lineCheck.ep1[1],lineCheck.ep2[1]) < self.h)&(max(lineCheck.ep1[1],lineCheck.ep2[1]) > self.h):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-
-        elif (self.type == 'v'):
-            if (lineCheck.type == 'h'):
-                if (min(self.ep1[1],self.ep2[1]) < lineCheck.h)&(max(self.ep1[1],self.ep2[1]) > lineCheck.h):
-                    if (min(lineCheck.ep1[0],lineCheck.ep2[0]) < self.v)&(max(lineCheck.ep1[0],lineCheck.ep2[0]) > self.v):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            elif (lineCheck.type == 'v'):
-                return False
-            else:
-                tempY = (self.v)*lineCheck.m + lineCheck.c
-                if (min(self.ep1[1],self.ep2[1]) < tempY)&(max(self.ep1[1],self.ep2[1]) > tempY):
-                    if (min(lineCheck.ep1[0],lineCheck.ep2[0]) < self.v)&(max(lineCheck.ep1[0],lineCheck.ep2[0]) > self.v):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-
-        else:
-            if (lineCheck.type == 'h'):
-                tempX = float((lineCheck.h - self.c)/self.m)
-                if (min(lineCheck.ep1[0],lineCheck.ep2[0]) < tempX)&(max(lineCheck.ep1[0],lineCheck.ep2[0]) > tempX):
-                    if (min(self.ep1[1],self.ep2[1]) < lineCheck.h)&(max(self.ep1[1],self.ep2[1]) > lineCheck.h):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-
-            elif (lineCheck.type == 'v'):
-                tempY = (lineCheck.v)*self.m + self.c
-                if (min(lineCheck.ep1[1],lineCheck.ep2[1]) < tempY)&(max(lineCheck.ep1[1],lineCheck.ep2[1]) > tempY):
-                    if (min(self.ep1[0],self.ep2[0]) < lineCheck.v)&(max(self.ep1[0],self.ep2[0]) > lineCheck.v):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                tempX = (lineCheck.c - self.c)/(self.m - lineCheck.m);
-                tempY = (self.m)*tempX + self.c;
-                if (min(lineCheck.ep1[1],lineCheck.ep2[1]) < tempY)&(max(lineCheck.ep1[1],lineCheck.ep2[1]) > tempY):
-                    if(min(lineCheck.ep1[0],lineCheck.ep2[0]) < tempX)&(max(lineCheck.ep1[0],lineCheck.ep2[0]) > tempX):
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-
-
+    # assert 1==2f
 
 # function that reads the obstacle file and returns line of obstacles
-
 def readObs(filename):
     # opening the file
     obsFile = open(filename, 'r')
@@ -259,10 +114,7 @@ def readObs(filename):
                 theLine = Line((corners[x][0], corners[x][1]), (corners[x+1][0], corners[x+1][1]))
                 obstacles.append(theLine)
 
-
-
     return obstacles
-
 
 
 if __name__ == "__main__":
@@ -282,7 +134,7 @@ if __name__ == "__main__":
     # assert 1 == 12
 
     st = start
-    for x in range(200):    
+    for x in range(200):
         end = get_rand(st);
         theLine = Line((st[0], st[1]),(end[0],end[1]))
 
@@ -296,14 +148,3 @@ if __name__ == "__main__":
             st = end
 
     plt.show()
-
-    # print "i will not be pritned"
-    # plt.show()
-
-
-
-
-
-
-
-
